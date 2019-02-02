e8d0a94f22319c58904e5d00fd4fa7a960934e9e
Serhii Ohorodnyk <serhii@serhiis-mbp.localdomain>
2019-02-02 12:14:38 +0100

    initial now deployment setup

---
+++ src/ssr.ts
@@ -0,0 +1,27 @@
+import { createServer, IncomingMessage, ServerResponse } from "http"
+
+const handler = async (req: IncomingMessage, res: ServerResponse) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(`
+    <html lang="en">
+    <head>
+      <title>Skiller</title>
+      <meta charset="utf8">
+      <meta name="viewport" content="user-scalable=0, initial-scale=1, width=device-width">
+      <link rel="shortcut icon" href="/favicon.ico" />
+    </head>
+    <body>
+      <h1>Skiller</h1>
+      <div>${req.url}</div>
+    </body>
+    </html>
+  `)
+}
+
+if (!process.env.IS_NOW) {
+  createServer(handler).listen(3000);
+  // tslint:disable-next-line:no-console
+  console.log("App is running: http://localhost:3000")
+}
+
+export default handler
