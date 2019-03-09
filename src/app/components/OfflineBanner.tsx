75c8534d3d304be935d412432cc74fd56f9daddc
Serhii Ohorodnyk <serhii.ohorodnyk@storecast.de>
2019-03-09 22:22:31 +0100

    add offline banner

---
+++ src/app/components/OfflineBanner.tsx
@@ -0,0 +1,33 @@
+import { Box, Layer, Text } from "grommet";
+import React from "react";
+import useNetwork from "react-use/lib/useNetwork";
+
+const OfflineBanner: React.FC = () => {
+  const network = useNetwork();
+  // Layer cannot be rendered in ssr, there we need to prevent it when online status is not defined
+  if (network.online === undefined) {
+    return null;
+  }
+  return (
+    <Layer
+      position={network.online ? "hidden" : "bottom"}
+      full="horizontal"
+      responsive={false}
+      modal={false}
+    >
+      <Box
+        direction="row"
+        justify="center"
+        align="center"
+        background="status-warning"
+        pad="medium"
+        fill={true}
+        basis="full"
+      >
+        <Text>Application is running in offline mode</Text>
+      </Box>
+    </Layer>
+  );
+};
+
+export default OfflineBanner;
--- src/app/views/Layout/Layout.tsx
+++ src/app/views/Layout/Layout.tsx
@@ -1,15 +1,34 @@
-import { Button } from "grommet";
+import { Button, Heading } from "grommet";
 import React from "react";
 import { hot } from "react-hot-loader/root";
+import { Route, Switch } from "react-router";
+import { Link } from "react-router-dom";
 
+import OfflineBanner from "app/components/OfflineBanner";
 import { LayoutContainer, Logo } from "./styles";
+
+const Asd = () => (
+  <Link to="/">
+    <Button primary={true} label="Home" />
+  </Link>
+);
+
+const Home = () => (
+  <Link to="/asd">
+    <Button primary={true} label="Asd" />
+  </Link>
+);
 
 const Layout: React.FC = () => {
   return (
     <LayoutContainer>
-      <h1>Skiller</h1>
+      <Heading level="1">Skiller</Heading>
       <Logo />
-      <Button primary={true} label="Skiller" />
+      <Switch>
+        <Route path="/asd" component={Asd} />
+        <Route component={Home} />
+      </Switch>
+      <OfflineBanner />
     </LayoutContainer>
   );
 };
